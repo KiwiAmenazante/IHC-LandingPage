f5f292a95338421df7963c5ad04699fd3b924f7b
fredy nicolas cervantes <u202110140@upc.edu.pe>
2024-05-22 18:57:47 -0500

    refactor: Implemenacion de diseño responsive

--- js/main.js
+++ js/main.js
@@ -1,20 +1,8 @@
-var images = document.querySelectorAll('.carousel-image');
-var currentImage = 0;
-
-function carousel() {
-    images[currentImage].classList.remove('active');
-    currentImage = (currentImage + 1) % images.length;
-    images[currentImage].classList.add('active');
-}
-
-document.getElementById('prev').addEventListener('click', function() {
-    images[currentImage].classList.remove('active');
-    currentImage = (currentImage - 1 + images.length) % images.length;
-    images[currentImage].classList.add('active');
-});
-
-document.getElementById('next').addEventListener('click', function() {
-    carousel();
-});
-
-setInterval(carousel, 3000); 
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
